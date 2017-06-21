7c0c6a25d37b6a4ce1f397513d55b8371982ddc8
kw7oe <choongkwern@hotmail.com>
2017-06-21 21:42:21 +0800

    Add in all degree

--- main_cf.pl
+++ main_cf.pl
@@ -32,6 +32,10 @@ logical_thinking(RulesCF) :-
 % =======
 % Subject
 % =======
+
+% ------------------
+% Subject: Computing
+% ------------------
 subject(computing, CombinedCF, E) :-
   logical_thinking(CF1),
   physics(no),
@@ -42,6 +46,7 @@ subject(computing, CombinedCF, E) :-
   calculate_cf([CF1, CF2], 100, CombinedCF),
   E = 'You are a logical person and don\'t like physics. \c 
   However, you are good in maths and solving problems.'.
+
 subject(computing, CombinedCF, E) :-
   solving_problem(CF1),
   work_with_numbers(no),
@@ -49,12 +54,48 @@ subject(computing, CombinedCF, E) :-
   calculate_cf([CF1], 85, CombinedCF),
   E = 'You are good in maths and solving problems. \c
   You also prefer to work on a computer.'.
+
 subject(computing, CombinedCF, E) :-
+  computer_or_hands(computer),
   blogs(technology),
   calculate_cf([100], 70, CombinedCF),
   E = 'You perfer working on a computer and like reading \c 
   blogs related to technology.'.
 
+% Degree: Computer Science
+degree(computer_science) :- 
+  subject(computing, CF1, E), !,
+  computer_systems(CF2),
+  technology(CF3),
+  write('CF1: '), write(CF1),
+  write('CF2: '), write(CF2),
+  write('CF3: '), write(CF3),
+  calculate_cf([CF1, CF2, CF3], 100, CF),
+  nl, write('Recommendation: Computer Science '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Information Technology
+degree(information_technology) :- 
+  subject(computing, CF1, E),
+  technology(CF3),
+  like_interact(CF2),
+  planning(yes),
+  calculate_cf([CF1, CF2], 100, CF),
+  nl, write('Recommendation: Information Technology '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+degree(information_technology) :-
+  subject(computing, CF1, E),
+  calculate_cf([CF1], 70, CF),
+  nl, write('Recommendation: Information Technology '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% --------------------
+% Subject: Engineering
+% --------------------
 subject(engineering, CombinedCF, E) :-
   logical_thinking(CF1),
   science(yes),
@@ -65,6 +106,7 @@ subject(engineering, CombinedCF, E) :-
   calculate_cf([CF1, CF2], 100, CombinedCF),
   E = 'You love science \c 
   and like to work with numbers.'.
+
 subject(engineering, CombinedCF, E) :- 
   science(yes),
   blogs(science),
@@ -75,6 +117,7 @@ subject(engineering, CombinedCF, E) :-
   E = 'You love science \c 
   and like to work with numbers.'.
 
+% Subject: Science
 subject(science, CombinedCF, E) :-
   logical_thinking(CF1),
   science(yes),
@@ -85,97 +128,193 @@ subject(science, CombinedCF, E) :-
   calculate_cf([CF1, CF2], 90, CombinedCF),
   E = 'You love science, prefer theory \c 
   and like to work with numbers.'.
+
 subject(science, CombinedCF, E) :-
   science(yes),
   theory_or_pratical(theory),
   calculate_cf([100], 80, CombinedCF),
   E = 'You love science and prefer theory.'.
 
-
-degree(computer_science) :-
-  subject(computing, CF1, E),
-  technology(CF2),
-  calculate_cf([CF1, CF2], 90, CF),
-  write('Recommendation: Computer Science '), 
-  write('(cf '), write(CF), write(')'), nl,
-  write(E).
-degree(computer_science) :-
-  subject(computing, CF1, E),
-  computer_systems(CF2),
-  calculate_cf([CF1, CF2], 90, CF),
-  write('Recommendation: Computer Science '), 
-  write('(cf '), write(CF), write(')'), nl,
-  write(E).
-degree(computer_science) :- 
-  subject(computing, CF1, E),
-  computer_systems(CF2),
-  technology(CF3),
-  calculate_cf([CF1, CF2, CF3], 100, CF),
-  write('Recommendation: Computer Science '), 
-  write('(cf '), write(CF), write(')'), nl,
-  write(E).
-
-degree(information_technology) :- 
-  subject(computing, CF1, E),
-  technology(apply),
+% Subject: Business
+subject(business, CombinedCF, E) :-  
   like_interact(yes),
+  dealing_with_people(CF1),  
   planning(yes),
-  calculate_cf([CF1], 100, CF),
-  write('Recommendation: Information Technology '), 
-  write('(cf '), write(CF), write(')'), nl,
-  write(E).
-degree(information_technology) :-
-  subject(computing, CF1, E),
-  calculate_cf([CF1], 90, CF),
-  write('Recommendation: Information Technology '), 
-  write('(cf '), write(CF), write(')'), nl,
-  write(E).
+  risk(CF2),
+  calculate_cf([CF1, CF2], 80, CombinedCF),
+  E = 'You are good in planning.'.
 
-% Engineering
+subject(business, CombinedCF, E) :-
+  like_interact(yes),
+  dealing_with_people(CF1),
+  blogs(business),
+  planning(yes),
+  calculate_cf([CF1, 100], 80, CombinedCF),
+  E = 'You like to interact with people.'.
+
+% Subject: Art
+subject(art, CombinedCF, E) :- 
+  imagination(CF1),
+  science(no),
+  creative_artistic_musical(yes),
+  work_with_numbers(no),
+  going_museum(yes),
+  calculate_cf([CF1, 100], 100, CombinedCF),
+  E = 'You like to interact with people.'.
+
+subject(art, CombinedCF, E) :-
+  science(no),
+  creative_artistic_musical(yes),
+  work_with_numbers(no),
+  calculate_cf([100], 90, CombinedCF),
+  E = 'You like to interact with people.'.
+
+% Subject: Hospitality
+subject(hospitality, CombinedCF, E) :-
+  computer_or_hands(hands),  
+  like_interact(yes),  
+  planning(yes),
+  service_minded(yes),
+  serving_people(yes),
+  calculate_cf([100], 90, CombinedCF),
+  E = 'You like to interact with people.'.
+
+subject(hospitality, CombinedCF, E) :-
+  computer_or_hands(hands), 
+  service_minded(yes),
+  serving_people(yes),
+  calculate_cf([100], 90, CombinedCF),
+  E = 'You like to interact with people.'.
+
+subject(hospitality, CombinedCF, E) :-
+  computer_or_hands(hands),
+  serving_people(yes),
+  calculate_cf([100], 90, CombinedCF),
+  E = 'You like to interact with people.'.
+
+% Degree: Electrical Engineering
 degree(electrical_engineering) :-
   subject(engineering, CF1, E),
   physics(yes),
   circuits(yes),
   calculate_cf([CF1], 90, CF),
-  write('Recommendation: Electrical Engineering '), 
+  nl, write('Recommendation: Electrical Engineering '), 
   write('(cf '), write(CF), write(')'), nl,
   write(E).
+
+% Degree: Mechanical Engineering
 degree(mechanical_engineering) :-
   subject(engineering, CF1, E),
   physics(yes),
   calculate_cf([CF1], 90, CF),
-  write('Recommendation: Mechanical Engineering '), 
+  nl, write('Recommendation: Mechanical Engineering '), 
   write('(cf '), write(CF), write(')'), nl,
   write(E).
+
+% Degree: Chemical Engineering
 degree(chemical_engineering) :-
   subject(engineering, CF1, E),
   chemistry(yes),
   calculate_cf([CF1], 90, CF),
-  write('Recommendation: Chemical Engineering '), 
+  nl, write('Recommendation: Chemical Engineering '), 
   write('(cf '), write(CF), write(')'), nl,
   write(E).
 
-% Science
+% Degree: Biotechnology
 degree(biotechnology) :-
   subject(science, CF1, E),
   biology(yes),
   genetic_engineering(yes),
   calculate_cf([CF1], 90, CF),
-  write('Recommendation: Biotechnology '), 
+  nl, write('Recommendation: Biotechnology '), 
   write('(cf '), write(CF), write(')'), nl,
   write(E).
+
+% Degree: Pure Science
 degree(pure_science) :- 
   subject(science, CF1, E),
   calculate_cf([CF1], 90, CF),
-  write('Recommendation: Pure Science '), 
+  nl, write('Recommendation: Pure Science '), 
   write('(cf '), write(CF), write(')'), nl,
   write(E).
+
+% Degree: Marketing
+degree(marketing) :-
+  subject(business, CF1, E),
+  storytelling(yes),
+  calculate_cf([CF1], 90, CF),
+  nl, write('Recommendation: Marketing '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Accounting
+degree(accounting) :-
+  subject(business, CF1, E),
+  work_with_numbers(yes),
+  detail_oriented(yes),
+  calculate_cf([CF1], 90, CF),
+  nl, write('Recommendation: Accounting '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Business Management
+degree(business_management) :-
+  subject(business, CF1, E),
+  calculate_cf([CF1], 90, CF),
+  nl, write('Recommendation: Business Management '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Performing Art
+degree(performing_art) :-
+  subject(art, CF1, E),
+  center_of_attention(yes),
+  film_or_perform(perform),
+  performing(yes),  
+  calculate_cf([CF1], 90, CF),
+  nl, write('Recommendation: Performing Art '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Digital Film Production
+degree(digital_film_production) :-
+  subject(art, CF1, E),
+  film_or_perform(film),
+  film(yes),  
+  calculate_cf([CF1], 90, CF),
+  nl, write('Recommendation: Digital Film Production '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Culinary Art
+degree(culinary_art) :- 
+  subject(hospitality, CF1, E),
+  cook(yes),  
+  calculate_cf([CF1], 100, CF),
+  nl, write('Recommendation: Culinary Art '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Hotel Management
+degree(hotel_management) :- 
+  subject(hospitality, CF1, E),  
+  calculate_cf([CF1], 100, CF),
+  nl, write('Recommendation: Hotel Management '), 
+  write('(cf '), write(CF), write(')'), nl,
+  write(E).
+
+% Degree: Gap Year
+degree(gap_year) :-
+  nl, write('Recommendation: Gap Year '), 
+  write('(cf '), write('Unknown'), write(')'), nl,
+  write('Sorry. We cannot help you because you have a \c
+    a variety of traits.').
 
 % ===========
 % Uncertainty
 % ===========
 calculate_cf(CFList, CF, RulesCF) :-
-  min_in_list(CFList, Min), !,
+  min_in_list(CFList, Min),
   RulesCF is div(Min * CF, 100).
 
 min_in_list([Min],Min). 
@@ -192,75 +331,87 @@ min_in_list([H,K|T],M) :-
 
 % CF 100
 question(science) :-
-  write('Do you like Chemistry/Biology/Physics?'), nl.
+  nl, write('Do you like Chemistry/Biology/Physics?'), nl.
 question(physics) :-
-  write('Do you enjoy doing Physics?'), nl.
+  nl, write('Do you enjoy doing Physics?'), nl.
 question(maths) :-
-  write('Are you good at Maths?'), nl.
+  nl, write('Are you good at Maths?'), nl.
 question(computer_or_hands) :-
-  write('Do you prefer working on a computer or working with your hands?'), nl.
+  nl, write('Do you prefer working on a computer or working with your hands?'), nl.
 question(work_with_numbers) :-
-  write('Do you like working with numbers?'), nl.
+  nl, write('Do you like working with numbers?'), nl.
 question(blogs) :-
-  write('If given a choice, which blogs do you prefer to read?'), nl.
+  nl, write('If given a choice, which blogs do you prefer to read?'), nl.
 question(theory_or_pratical) :-
-  write('Do you want to discover the next new element or to build the next big invention?'), nl.
+  nl, write('Do you want to discover the next new element or to build the next big invention?'), nl.
 question(challenge_yourself) :-
-  write('Do you like to challenge yourself?'), nl.
+  nl, write('Do you like to challenge yourself?'), nl.
+question(planning) :-
+  nl, write('Do you like planning, organising or managing?'), nl.
+question(creative_artistic_musical) :-
+  nl, write('Are you creative or artistic or musical?'), nl.
+question(going_museum) :-
+  nl, write('Do you enjoy going to movie theater and museums?'), nl.
+question(service_minded) :-
+  nl, write('Are you service minded with a high stress threshold and want to work with people?'), nl.
+question(serving_people) :-
+  nl, write('I dont mind serving people.'), nl.
 
 % CF Unknown
 question(logic) :-
-  write('Are you a person of logic?'), nl.
-question(imagination) :-
-  write('Are you a person of imagination?'), nl.
+  nl, write('Are you a person of logic?'), nl.
+question(imaginatiion) :-
+  nl, write('Are you a person of imagination?'), nl.
 question(rational) :-
-  write('Are u a rational person?'), nl.
+  nl, write('Are u a rational person?'), nl.
 question(solving_problem) :-
-  write('Rate 1 to 5, where 1 refers to weak and 5 refers to excellent, '), nl,
+  nl, write('Rate 1 to 5, where 1 refers to weak and 5 refers to excellent, '), nl,
   write('How good your problem solving skill is?'), nl.
+question(risk) :-
+  nl, write('I like to take risk'), nl.
+question(dealing_with_people) :-
+  nl, write('Rate 1 to 5, where 1 refers to weak and 5 refers to excellent, '), nl,
+  write('How good are you in dealing with people?'), nl.
+question(like_interact) :-
+  nl, write('I like interacting with people.'), nl.
 
 % Computer Science
 question(computer_systems) :-
-  write('I am interested in the details of how computer systems or software works.'), nl.
+  nl, write('I am interested in the details of how computer systems or software works.'), nl.
 question(technology) :-
-  write('I prefer to develop technology rather than apply technology.'), nl.
+  nl, write('I prefer to develop technology rather than apply technology.'), nl.
+
 % Question for Engineering
 question(circuits) :-
-  write('Do you like to deal with circuits?'), nl.
-
+  nl, write('Do you like to deal with circuits?'), nl.
 question(chemistry) :-
-  write('Do you like Chemistry?'), nl.
+  nl, write('Do you like Chemistry?'), nl.
 
 % Question for Science 
 question(biology) :-
-  write('Do you like Biology?'), nl.
-
+  nl, write('Do you like Biology?'), nl.
 question(genetic_engineering) :-
-  write('Do you find genetic engineering intersting?'), nl.
+  nl, write('Do you find genetic engineering intersting?'), nl.
 
 % Question for Business
 question(detail_oriented) :-
-  write('Are you detail oriented and pay attention to little things?'), nl.
-
+  nl, write('Are you detail oriented and pay attention to little things?'), nl.
 question(storytelling) :-
-  write('Are you good at storytelling?'), nl.
+  nl, write('Are you good at storytelling?'), nl.
 
 % Question for Arts
 question(center_of_attention) :-
-  write('I like being the center of attention.'), nl.
-
+  nl, write('I like being the center of attention.'), nl.
 question(film_or_perform) :-
-  write('Do you prefer shooting film or performing?'), nl.
-
+  nl, write('Do you prefer shooting film or performing?'), nl.
 question(film) :-
-  write('Are you interested in the arts of storytelling and the language of film?'), nl.
-
+  nl, write('Are you interested in the arts of storytelling and the language of film?'), nl.
 question(performing) :-
-  write('I prefer to convey artistic expression through acting/dancing/singing.'), nl.
+  nl, write('I prefer to convey artistic expression through acting/dancing/singing.'), nl.
 
 % Question for Hospitality
 question(cook) :-
-  write('Do you enjoy cooking?'), nl.
+  nl, write('Do you enjoy cooking?'), nl.
 
 % ========
 % Answers
@@ -268,52 +419,41 @@ question(cook) :-
 answer(computer) :-
   write('I prefer working on a computer.').
 answer(hands) :-
-  write('I prefer working with your hands.').
-answer(yes) :-
-  write('Yes.').
-answer(no) :-
-  write('No.').
+  write('I prefer working with your hands.'), nl.
+
 answer(business) :- 
   write('Forbes or BusinessInsider.').
 answer(technology) :-
   write('HackerNews or AnandTech.').
 answer(science) :-
-  write('howstuffworks or New Scientist.').
+  write('howstuffworks or New Scientist.'), nl.
+
 answer(theory) :-
   write('I am more interested to discover the next new element.').
 answer(pratical) :-
-  write('I am more interested to invent the next big invention.').
+  write('I am more interested to invent the next big invention.'), nl.
 
+answer(film) :-
+  write('I prefer shooting film.').
+answer(perform) :-
+  write('I prefer performing.'), nl.
+
+answer(yes) :-
+  write('Yes.').
+answer(no) :-
+  write('No.'), nl.
 
 answer(strongly_disagree) :-
   write('Strongly disagree.').
-
 answer(disagree) :-
   write('Disagree.').
-
 answer(neutral) :-
   write('Neutral').
-
 answer(agree) :-
   write('Agree.').
-
 answer(strongly_agree) :-
-  write('Strongly agree.').
+  write('Strongly agree.'), nl.
 
-answer(-100) :-
-  write(1).
-
-answer(-50) :-
-  write(2).
-
-answer(0) :-
-  write(3).
-
-answer(50) :-
-  write(4).
-
-answer(100) :-
-  write(5).
 
 % ==========================
 % Assign Answer to Questions
@@ -363,7 +503,32 @@ challenge_yourself(Answer) :-
 challenge_yourself(Answer) :-
   \+ progress(challenge_yourself, _),
   ask(challenge_yourself, Answer, [yes, no]).
+planning(Answer) :-
+  progress(planning, Answer).
+planning(Answer) :-
+  \+ progress(planning, _),
+  ask(planning, Answer, [yes, no]).
+serving_people(Answer) :-
+  progress(serving_people, Answer).
+serving_people(Answer) :-
+  \+ progress(serving_people, _),
+  ask(serving_people, Answer, [yes, no]).
+service_minded(Answer) :-
+  progress(service_minded, Answer).
+service_minded(Answer) :-
+  \+ progress(service_minded, _),
+  ask(service_minded, Answer, [yes, no]).
+creative_artistic_musical(Answer) :-
+  progress(creative_artistic_musical, Answer).
+creative_artistic_musical(Answer) :-
+  \+ progress(creative_artistic_musical, _),
+  ask(creative_artistic_musical, Answer, [yes, no]).
 
+going_museum(Answer) :-
+  progress(going_museum, Answer).
+going_museum(Answer) :-
+  \+ progress(going_museum, _),
+  ask(going_museum, Answer, [yes, no]).
 
 % Engineering
 circuits(Answer) :-
@@ -460,7 +625,23 @@ technology(Answer) :-
   \+ progress(technology, _),
   ask_with_cf(technology, Answer, [strongly_disagree, disagree, neutral, agree, strongly_agree]).
 
+risk(Answer) :-
+  progress(risk, Answer).
+risk(Answer) :-
+  \+ progress(risk, _),
+  ask_with_cf(risk, Answer, [strongly_disagree, disagree, neutral, agree, strongly_agree]).
 
+dealing_with_people(Answer) :-
+  progress(dealing_with_people, Answer).
+dealing_with_people(Answer) :-
+  \+ progress(dealing_with_people, _),
+  ask_with_cf(dealing_with_people, Answer, [strongly_disagree, disagree, neutral, agree, strongly_agree]).
+
+like_interact(Answer) :-
+  progress(like_interact, Answer).
+like_interact(Answer) :-
+  \+ progress(like_interact, _),
+  ask_with_cf(like_interact, Answer, [strongly_disagree, disagree, neutral, agree, strongly_agree]).
 % Outputs a nicely formatted list of answers
 % [First|Rest] is the Choices list, Index is the index of First in Choices
 answers([], _).
@@ -491,5 +672,6 @@ ask_with_cf(Question, Answer, Choices) :-
   question(Question),
   answers(Choices, 1),
   read(Index),
-  asserta(progress(Question, Index * 20)),
+  CF is Index * 20,
+  asserta(progress(Question, CF)),
   Index = Answer.
